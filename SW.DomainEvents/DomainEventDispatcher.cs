77e1981d2368adec238caf0799f7147622bdadb6
Samer Awajan <samer@simplify9.com>
2020-02-29 11:24:37 +0200

    initial commit

---
+++ SW.DomainEvents/DomainEventDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SW.PrimitiveTypes;
+using System;
+using System.Threading.Tasks;
+
+namespace SW.DomainEvents
+{
+    internal class DomainEventDispatcher : IDomainEventDispatcher
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public DomainEventDispatcher(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        async public Task Dispatch(IDomainEvent domainEvent)
+        {
+            var handlers = serviceProvider.GetServices(typeof(IHandle<>).MakeGenericType(domainEvent.GetType()));
+
+            foreach (dynamic handler in handlers)
+                await handler.Handle(domainEvent);
+        }
+    }
+}
---
+++ SW.DomainEvents/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using SW.PrimitiveTypes;
+using System;
+using System.Reflection;
+
+namespace SW.DomainEvents
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDomainEvents(this IServiceCollection serviceCollection, params Assembly[] assemblies)
+        {
+            if (assemblies.Length == 0) assemblies = new Assembly[] { Assembly.GetCallingAssembly() };
+
+            serviceCollection.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(classes => classes.AssignableTo(typeof(IHandle<>)))
+                .AsImplementedInterfaces().WithScopedLifetime());
+
+            serviceCollection.AddScoped<DomainEventDispatcher>();
+
+            return serviceCollection;
+        }
+
+    }
+}
